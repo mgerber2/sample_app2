9af1a951551517882b7ce8586700d58e9d203fd0
Michael Gerber <mgerber2@comcast.net>
2014-03-02 20:29:05 -0400

    tests

--- test/integration/static_pages_test.rb
+++ test/integration/static_pages_test.rb
@@ -4,4 +4,20 @@ class StaticPagesTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "home page has content 'home'" do
+        get "/static_pages/home"
+        assert response.body.include?('home')
+  end
+  test "about page has content 'about'" do
+        get "/static_pages/home"
+        assert response.body.include?('about')
+  end
+  test "contact page has content 'contact'" do
+        get "/static_pages/contact"
+        assert response.body.include?('contact')
+  end
+  test "help page hast content 'help'" do
+        get "/static_pages/help"
+        assert response.body.include?('help')
+  end
 end
