9d6757f1798af2f3f79aed4f12d07209bb5491dc
Michael Gerber <mgerber2@comcast.net>
2014-03-02 21:27:59 -0400

    tests2

--- test/integration/static_pages_test.rb
+++ test/integration/static_pages_test.rb
@@ -8,16 +8,32 @@ class StaticPagesTest < ActionDispatch::IntegrationTest
         get "/static_pages/home"
         assert response.body.include?('home')
   end
-  test "about page has content 'about'" do
+  test "the title for home is not empty" do
         get "/static_pages/home"
+        assert_match !<title>Ruby on Rails Tutorial Sample App | Home</title>!i, response.body
+  end
+  test "about page has content 'about'" do
+        get "/static_pages/about"
         assert response.body.include?('about')
+  end
+  test "the title for about is not empty" do
+        get "/static_pages/about"
+        assert_match !<title>Ruby on Rails Tutorial Sample App | About</title>!i, response.body
   end
   test "contact page has content 'contact'" do
         get "/static_pages/contact"
         assert response.body.include?('contact')
   end
+  test "the title for contact is not empty" do
+        get "/static_pages/contact"
+        assert_match !<title>Ruby on Rails Tutorial Sample App | Contact</title>!i, response.body
+  end
   test "help page hast content 'help'" do
         get "/static_pages/help"
         assert response.body.include?('help')
+  end
+  test "the title for help is not empty" do
+        get "/static_pages/help"
+        assert_match !<title>Ruby on Rails Tutorial Sample App | Help</title>!i, response.body
   end
 end
